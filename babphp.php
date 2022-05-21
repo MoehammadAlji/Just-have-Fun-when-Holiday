305149f30825050512756d09d6cac116004a9339
Moehammad Rizky <mamankirfan@gmail.com>
2022-05-21 17:07:23 +0700

    test

---
+++ babphp.php
@@ -0,0 +1,51 @@
+<?php
+//NOMOR .1
+
+    // function lulusgaklulus($nilai){
+    //     if ($nilai >= 75) {
+    //         echo "LULUS \n";
+    //     }else{
+    //         echo "TIDAK LULUS \n";
+    //     }
+    // }
+
+    // lulusgaklulus(85);
+    // lulusgaklulus(75);
+    // lulusgaklulus(15);
+
+    //atau bisa juga
+
+//     $nilai = 75;
+// switch ($nilai) {
+//    	case $nilai >= 75:
+// 	echo "Lulus\n";
+//     break;
+// 	case $nilai < 75:
+// 		echo "Tidak lulus\n";
+// 		break;
+// 	default:
+// 		echo "Tidak valid\n";
+// 		break;
+// }
+
+//NOMOR 2.
+// $x =0;
+// while($x <= 100) {
+//     if ($x % 2 == 1){
+//         echo $x . "\n";
+//     }
+//     $x++;
+// }
+//NOMOR 3. 
+//  $namahari = ["Ahad", "Senin", "Selasa", "Rabu", "Kamis","Jum'at","sabtu"];
+
+//     echo $namahari[1] . "\n";
+//     echo $namahari[3] . "\n";
+//     echo $namahari[5] . "\n";
+//NOMOR 4.
+    // function kenalandong($namaku, $umurku, $hobbyku){
+    //     return "Hallo, namaku $namaku, aku berumur $umurku tahun... Hobbyku adalah $hobbyku \n";
+    // }
+
+    // echo kenalandong("Irfan", 16, "menonton film");
+?>
